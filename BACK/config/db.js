acb1d3e0b6cdc45e60596b49aea8ff60436e56f3
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2022-12-19 08:49:55 +0100

    Add files via upload

---
+++ BACK/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require ("mongoose");
+
+mongoose
+    .connect("mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.af3bwpu.mongodb.net/groupomania",
+        {
+    useNewUrlParser: true,
+    useUnifiedTopology : true,
+    // useCreateIndex : true,
+    // useFindAndModify: false,
+        }
+    )
+    .then(() => console.log('Successfully Connected to MongoDB !'))
+    .catch((err) => console.log('Oops - Failed to Connet to MongoDB', err));
+
+
+
+
