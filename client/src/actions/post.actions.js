165b619fa2344de1627e5fffae4c4819f781966f
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2022-12-27 20:22:02 +0100

    Update post.actions.js

--- client/src/actions/post.actions.js
+++ client/src/actions/post.actions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// posts
+//--Posts--
 export const GET_POSTS = "GET_POSTS";
 export const GET_ALL_POSTS = "GET_ALL_POSTS";
 export const ADD_POST = "ADD_POST";
@@ -9,15 +9,15 @@ export const UNLIKE_POST = "UNLIKE_POST";
 export const UPDATE_POST = "UPDATE_POST";
 export const DELETE_POST = "DELETE_POST";
 
-// comments
+//--Comments--
 export const ADD_COMMENT = "ADD_COMMENT";
 export const EDIT_COMMENT = "EDIT_COMMENT";
 export const DELETE_COMMENT = "DELETE_COMMENT";
 
-// trends
+//--Trends--
 export const GET_TRENDS = "GET_TRENDS";
 
-// errors
+//--Errors--
 export const GET_POST_ERRORS = "GET_POST_ERRORS";
 
 export const getPosts = (num) => {
