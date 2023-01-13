5916044dfd830d813fcfbaa3f6dc26d6556a6359
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2023-01-13 12:30:35 +0100

    Add files via upload

---
+++ frontend/src/pages/Profil.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import Log from "../components/Log";
+import { UidContext } from "../components/AppContext";
+import UpdateProfil from "../components/Profil/UpdateProfil";
+
+//--L'uid vérifie l'identité de la personne qui veux accéder au profil--
+const Profil = () => {
+  const uid = useContext(UidContext);
+
+  return (
+    <div className="profil-page">
+      {uid ? (
+        <UpdateProfil />
+      ) : (
+        <div className="log-container">
+          <Log signin={false} signup={true} />
+          <div className="img-container">
+            <img src="./img/log5.png" alt="img-log" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Profil;
