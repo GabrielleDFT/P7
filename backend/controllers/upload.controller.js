00d477e5cf422da95fb989688742708b5ce7a650
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2023-01-15 05:49:57 +0100

    Add files via upload

---
+++ backend/controllers/upload.controller.js
@@ -0,0 +1,37 @@
+const UserModel = require("../models/user.model");
+const fs = require("fs");
+const { uploadErrors } = require("../utils/errors.utils");
+const sharp = require("sharp");
+
+//---Upload image profil utilisateur---
+module.exports.uploadProfil = async (req, res) => {
+ 
+  //---Renomme le fichier avec extension .jpg---
+  const fileName = req.body.name +".jpg"; 
+  try {
+    if (
+      req.file.mimetype != "image/jpg" &&
+      req.file.mimetype != "image/png" &&
+      req.file.mimetype != "image/jpeg"
+    )
+      throw Error("invalid file");
+
+    if (req.file.size > 500000) throw Error("max size");
+  } catch (err) {
+    const errors = uploadErrors(err);
+    return res.status(201).json({ errors });
+  }
+
+  try {
+    await sharp(req.file.buffer)
+      .resize({ width: 150, height: 150 }) 
+      .toFile(`${__dirname}/../frontend/public/uploads/profil/${fileName}`
+      );
+    res.status(201).send("Photo de profil chargée avec succés");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+}
+
+
+
