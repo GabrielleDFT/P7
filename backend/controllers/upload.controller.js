f3e97a5683bffde2b09caf80465ccf859d72bc9d
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2023-01-16 05:34:20 +0100

    Update upload.controller.js

--- backend/controllers/upload.controller.js
+++ backend/controllers/upload.controller.js
@@ -1,5 +1,7 @@
+//----------------------------------------GESTION UPLOADS D'IMAGES---------------------------------------
+
 const UserModel = require("../models/user.model");
-const fs = require("fs");
+const fs = require("fs");//---Création de fichiers (incrémente éléments ds fichiers)---
 const { uploadErrors } = require("../utils/errors.utils");
 const sharp = require("sharp");
 
@@ -9,23 +11,23 @@ module.exports.uploadProfil = async (req, res) => {
   //---Renomme le fichier avec extension .jpg---
   const fileName = req.body.name +".jpg"; 
   try {
-    if (
+    if (//---Format autorisés des images---
       req.file.mimetype != "image/jpg" &&
       req.file.mimetype != "image/png" &&
       req.file.mimetype != "image/jpeg"
     )
       throw Error("invalid file");
 
-    if (req.file.size > 500000) throw Error("max size");
+    if (req.file.size > 500000) throw Error("max size");//---Size autorisé des images---
   } catch (err) {
     const errors = uploadErrors(err);
     return res.status(201).json({ errors });
   }
 
-  try {
+  try {//---Upload, Resize & stock new image avec Sharp---
     await sharp(req.file.buffer)
       .resize({ width: 150, height: 150 }) 
-      .toFile(`${__dirname}/../frontend/public/uploads/profil/${fileName}`
+      .toFile(`${__dirname}/../frontend/public/uploads/profil/${fileName}`//chemin où seront stockés les images
       );
     res.status(201).send("Photo de profil chargée avec succés");
   } catch (err) {
