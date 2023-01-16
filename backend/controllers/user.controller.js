ea927085243cc2ff61511fcec26462d95252c834
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2023-01-16 01:21:12 +0100

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,21 +1,28 @@
+//----------------------------------------GESTION USERS (+ user.routes)-----------------------------------------------
+
 const UserModel = require("../models/user.model");
+
+//---Controler à chq fois que les id st reconnus par la BDD---
 const ObjectID = require("mongoose").Types.ObjectId;
 
+//---Sélection des Infos de tous les Users (fonction déclarée dans user.routes)---
 module.exports.getAllUsers = async (req, res) => {
-  const users = await UserModel.find().select("-password");
+  const users = await UserModel.find().select("-password");//---Ne selectionne pas les passwords---
   res.status(200).json(users);
 };
 
+//---Sélection des Infos d'un User---
 module.exports.userInfo = (req, res) => {
-  if (!ObjectID.isValid(req.params.id))
+  if (!ObjectID.isValid(req.params.id))//---Test si l'id est connu dans BDD---
     return res.status(400).send("ID unknown : " + req.params.id);
 
-  UserModel.findById(req.params.id, (err, docs) => {
-    if (!err) res.send(docs);
+  UserModel.findById(req.params.id, (err, docs) => {//docs = data
+    if (!err) res.send(docs);//---Envoi la data si pas d'erreur dans BDD---
     else console.log("ID unknown : " + err);
-  }).select("-password");
+  }).select("-password");//---On ne voit tjrs pas le password---
 };
 
+//---Mise à jour Data User---
 module.exports.updateUser = async (req, res) => {
   if (!ObjectID.isValid(req.params.id))
     return res.status(400).send("ID unknown : " + req.params.id);
@@ -24,11 +31,11 @@ module.exports.updateUser = async (req, res) => {
     await UserModel.findOneAndUpdate(
       { _id: req.params.id },
       {
-        $set: {
+        $set: {//Changer/Editer
           bio: req.body.bio,
         },
       },
-      { new: true, upsert: true, setDefaultsOnInsert: true })
+      { new: true, upsert: true, setDefaultsOnInsert: true })//---Paramètres obligatoires lors d'un put---
       .then((data) => res.send(data))
       .catch((err) => res.status(500).send({ message: err }));
   } catch (err) {
@@ -36,22 +43,24 @@ module.exports.updateUser = async (req, res) => {
   }
 };
 
+//---Suppression User---
 module.exports.deleteUser = async (req, res) => {
   if (!ObjectID.isValid(req.params.id))
     return res.status(400).send("ID unknown : " + req.params.id);
 
   try {
-    await UserModel.remove({ _id: req.params.id }).exec();
-    res.status(200).json({ message: "Successfully deleted. " });
+    await UserModel.remove({ _id: req.params.id }).exec();//Supression
+    res.status(200).json({ message: "Successfully deleted " });
   } catch (err) {
     return res.status(500).json({ message: err });
   }
 };
 
+//---Mise à jour des Follows---
 module.exports.follow = async (req, res) => {
   if (
-    !ObjectID.isValid(req.params.id) ||
-    !ObjectID.isValid(req.body.idToFollow)
+    !ObjectID.isValid(req.params.id) ||//Vérification id ou
+    !ObjectID.isValid(req.body.idToFollow)//Controle
   )
     return res.status(400).send("ID unknown : " + req.params.id);
 
@@ -59,14 +68,12 @@ module.exports.follow = async (req, res) => {
     //---Add to Followers list---
     await UserModel.findOneAndUpdate(
       {_id: req.params.id},
-      { $addToSet: { following: req.body.idToFollow } }
+      { $addToSet: { following: req.body.idToFollow } }//---Rajoute 1 element à la data 
       )
-
       //---Add to Following list---
       await UserModel.findOneAndUpdate(
         req.body.idToFollow,
         { $addToSet: { followers: req.params.id } })
-        
           .then((data) => res.send(data))
          
   } catch (err) {
@@ -74,13 +81,13 @@ module.exports.follow = async (req, res) => {
   }
 };
 
+//---Mise à jour des Unfollows---
 module.exports.unfollow = async (req, res) => {
   if (
     !ObjectID.isValid(req.params.id) ||
     !ObjectID.isValid(req.body.idToUnfollow)
   )
     return res.status(400).send("ID unknown : " + req.params.id);
-
   try {
     await UserModel.findOneAndUpdate(
       {_id: req.params.id},
