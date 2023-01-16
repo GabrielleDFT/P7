91ec12e3a28d6c60f9d41f6dd211c1b83477b3b4
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2023-01-16 02:07:09 +0100

    Update user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,9 @@
+//----------------------------------------GESTION ROUTES-----------------------------------------------
+
 //---Module Router d'Express---
 const router = require("express").Router();
 
-//---Controllers---
+//---Routes Controllers---
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
 const uploadController = require('../controllers/upload.controller');
@@ -9,22 +11,23 @@ const uploadController = require('../controllers/upload.controller');
 const multer = require("multer");
 const upload = multer();
 
-//---Authentification---
+//---Routes Authentification---
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
 router.get("/logout", authController.logout);
 
-//---Users DB---
+//---Routes Users DB---
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.userInfo);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 
-//---Follow & Unfollow---
+//---Routes Follow & Unfollow---
 router.patch("/follow/:id", userController.follow);
 router.patch("/unfollow/:id", userController.unfollow);
 
-//---Upload---
+//---Route Upload---
 router.post("/upload", upload.single("file"), uploadController.uploadProfil);
 
 module.exports = router;
+
