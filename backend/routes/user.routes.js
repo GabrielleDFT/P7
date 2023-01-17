7a4f0a9b164645270dd96d0eb401b5ae090ae878
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2023-01-17 16:12:52 +0100

    Update user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -18,19 +18,19 @@ const multer = require("multer");
 const upload = multer();
 
 //---Routes Authentification---
-router.post("/register", requireAuth, authController.signUp);
-router.post("/login", requireAuth, authController.signIn);
-router.get("/logout", requireAuth, authController.logout);
+router.post("/register", checkUser, authController.signUp);
+router.post("/login", checkUser, authController.signIn);
+router.get("/logout", checkUser, authController.logout);
 
 //---Routes Users DB---
-router.get("/", requireAuth, userController.getAllUsers);
-router.get("/:id", requireAuth, userController.userInfo);
-router.put("/:id", requireAuth, userController.updateUser);
-router.delete("/:id", requireAuth, userController.deleteUser);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 //---Routes Follow & Unfollow---
-router.patch("/follow/:id",  requireAuth, userController.follow);
-router.patch("/unfollow/:id",  requireAuth, userController.unfollow);
+router.patch("/follow/:id",  checkUser, userController.follow);
+router.patch("/unfollow/:id",  checkUser, userController.unfollow);
 
 //---Route Upload---
 router.post("/upload", upload.single("file"), uploadController.uploadProfil);
